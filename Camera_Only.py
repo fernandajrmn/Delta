de84d20a222019c8f685653562ef033db8442ae4
fernandajrmn <fernandajrmn@gmail.com>
2026-06-09 23:36:03 -0600

    Codigo de camara con funcion chida para leerla.

--- Camera_Only.py
+++ Camera_Only.py
@@ -18,31 +18,71 @@ print("Cognex connected")
 
 
 
+def safe_float(value, default=0.0):
+    try:
+        value = value.strip()
+        if value == "":
+            return default
+        return float(value)
+    except:
+        return default
+
     
-        
-print(len("1,0.1038,-0.0220,0,0,N,53.2007"))
+def read_cognex():
+    data = ""
+
+    while True:
+        c = camera.recv(1).decode(errors="ignore")
+        if c == "<":
+            break
+
+    while True:
+        c = camera.recv(1).decode(errors="ignore")
+        if c == ">":
+            break
+        data += c
+
+    print("RAW:", repr(data))
+
+    values = data.split("|")
+
+    if len(values) != 7:
+        return None
+
+    return {
+        "label": values[0].strip(),
+        "x": safe_float(values[1]),
+        "y": safe_float(values[2]),
+        "blob_status": values[3].strip(),
+        "logo_status": values[4].strip(),
+        "ocr_text": values[5].strip(),
+        "mean_color": safe_float(values[6]),
+    }
 
 while True:
     
-    data = camera.recv(32).decode(errors="ignore").strip()
+    # data = camera.recv(128).decode(errors="ignore").strip()
     
   
-    values = data.split(",")
+    # values = data.split(",")
     
-    while len(values) < 7:
-        values.append("0")
+    # while len(values) < 7:
+    #     values.append("0")
         
-    result = {
-                "label": values[0].strip().upper(),
-                "x": float(values[1]),
-                "y": float(values[2]),
-                "blob_status": values[3].strip().upper(),
-                "logo_status": values[4].strip().upper(),
-                "ocr_text": values[5].strip().upper(),
-                "mean_color": float(values[6]),
-            }
+    # result = {
+    #             "label": values[0].strip().upper(),
+    #             "x": float(values[1]),
+    #             "y": float(values[2]),
+    #             "blob_status": values[3].strip().upper(),
+    #             "logo_status": values[4].strip().upper(),
+    #             "ocr_text": values[5].strip().upper(),
+    #             "mean_color": float(values[6]),
+    #         }
     
-    print(result)
+    # print(data)
+    
+    results = read_cognex()
+    
     
     
     sleep(0.05)
